814295127e17e3c0dc3a7d48e11d9bfff06040e3
David Huertas <davidhuertasortiz@gmail.com>
2023-03-23 22:56:57 +0100

    feat: add env

--- src/functions.ts
+++ src/functions.ts
@@ -1,4 +1,4 @@
 export const getTodos = async (user: string) => {
-  const res = await fetch(`http://localhost:8000/get/${user}`)
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/get/${user}`)
   return res.json()
 }
